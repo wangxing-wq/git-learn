b0d056dff044601226329664d844c1ee275b651f
weishengwx <2234346829@qq.com>
2022-12-26 21:24:11 +0800

    fix 增加slave branch 一个类

---
+++ src/main/java/com/wx/GitSlaveBranch.java
@@ -0,0 +1,13 @@
+package com.wx;
+
+/**
+ * @author 22343
+ * @version 1.0
+ */
+public class GitSlaveBranch {
+	
+	public static void main(String[] args) {
+		System.out.println("GitSlaveBranch");
+	}
+	
+}
