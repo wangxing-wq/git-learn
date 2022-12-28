5b6402d036636830516d1c7c9b03fe08d5a43ac5
weishengwx <2234346829@qq.com>
2022-12-28 21:40:34 +0800

    修改

--- src/main/java/com/wx/App.java
+++ src/main/java/com/wx/App.java
@@ -5,6 +5,10 @@ package com.wx;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("1.O 分支");
+		System.out.println("1.O 分支 slave");
+		System.out.println("1.O 分支 slave");
+		System.out.println("1.O 分支 slave");
+		System.out.println("1.O 分支 slave");
+		System.out.println("1.O 分支 slave");
 	}
 }
